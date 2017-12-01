afbfa07f1e1e76a254c091652e9f1a2ffe4e3a7d
sasgra <saschagranberg.jobb@gmail.com>
2017-12-01 13:00:20 +0100

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-from distutils.core import setup
+# encoding: utf-8
+from setuptools import setup
+
+def readme():
+    """Import README for use as long_description."""
+    with open("README.rst") as f:
+        return f.read()
+
+version = "0.0.1"
 
 setup(
-  name = 'kolada_scraper',
-  packages = ['kolada'],
-  version = '0.1',
-  description = 'A scraper of statistical data from api.kolada.se/v2 built on top of Statscraper.',
-  author = 'Sascha Granberg',
-  author_email = 'saschagranberg.jobb@gmail.com',
-  url = 'URL to the scraper repository.',
-  download_url = 'Download URL',
+    name="kolada_scraper",
+    version=version,
+    description="A scraper of statistical data from api.kolada.se/v2 built on top of Statscraper.",
+    long_description=readme(),
+    url="https://github.com/horriblesmell/kolada-scraper",
+    author="Sascha Granberg",
+    author_email="saschagranberg.jobb@gmail.com",
+    license="MIT",
+    packages=["kolada"],
+    zip_safe=False,
+    install_requires=[
+        "requests",
+    ],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    include_package_data=True,
+    download_url="https://github.com/horriblesmell/kolada-scraper/archive/%s.tar.gz"
+                 % version,
 )
